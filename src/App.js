e1edba2961273bad861353384eebcbbf595b50a7
JiokKae <ekaktm1@gmail.com>
2022-10-31 07:43:03 +0900

    Router 적용

--- src/App.js
+++ src/App.js
@@ -1,4 +1,12 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./routers/Home";
 
 export default function App() {
-	return <div></div>;
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<Home />} />
+			</Routes>
+		</BrowserRouter>
+	);
 }
