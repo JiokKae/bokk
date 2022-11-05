bc617d70b3a56273f3a7b7ca8c48f3929dcd6a2b
JiokKae <ekaktm1@gmail.com>
2022-11-05 20:04:10 +0900

    Header와 HeaderLayout을 병합

--- src/outlets/HeaderLayout.js
+++ src/outlets/HeaderLayout.js
@@ -1,11 +1,22 @@
 import { Outlet } from "react-router-dom";
-import Header from "../components/Header/Header";
+import SearchSelector from "../components/Header/SearchSelector/SearchSelector";
+import Navbar from "../components/Header/Navbar/Navbar";
+import styles from "./HeaderLayout.module.css";
 
 export default function HeaderLayout() {
+	const NAV_ITEMS = [
+		{ name: "웹 링크", url: "" },
+		{ name: "전적 검색", url: "gameRecordSearch/" },
+		{ name: "게시판", url: "messageboard/" },
+		{ name: "다운로드", url: "download/" },
+	];
 	return (
 		<div>
-			<Header />
+			<Navbar items={NAV_ITEMS} />
 			<div className="container">
+				<div className={styles.header}>
+					<SearchSelector />
+				</div>
 				<div className="m-4">
 					<Outlet />
 				</div>
