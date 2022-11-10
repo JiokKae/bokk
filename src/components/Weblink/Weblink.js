deec949c0ec3a09e1d91186467700725cbaa6edc
JiokKae <ekaktm1@gmail.com>
2022-11-11 01:57:57 +0900

    Weblink component에 style, onClick 속성 추가

--- src/components/Weblink/Weblink.js
+++ src/components/Weblink/Weblink.js
@@ -1,12 +1,20 @@
 import styles from "./Weblink.module.css";
 
-export default function Weblink({ name, url, color, backgroundColor }) {
+export default function Weblink({
+	name,
+	url,
+	color,
+	backgroundColor,
+	style,
+	onClick,
+}) {
 	return (
 		<a
-			href={url}
+			href={onClick ? undefined : url}
 			target="_blank"
 			className={`btn ${styles.btnMd} m-1`}
-			style={{ color, backgroundColor }}>
+			style={{ color, backgroundColor, ...style }}
+			onClick={onClick}>
 			{name}
 		</a>
 	);
