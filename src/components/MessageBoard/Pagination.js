5704a6ad17d491da313c28e309e0a2270e4f04d0
JiokKae <ekaktm1@gmail.com>
2023-01-19 12:54:40 +0900

    코드 개선

--- src/components/MessageBoard/Pagination.js
+++ src/components/MessageBoard/Pagination.js
@@ -1,49 +1,52 @@
 import styled from "styled-components";
 
 function Numbers({ boardPage, totalPage, setCurrentPage }) {
+	const numberButtonOnClick = (index) => {
+		if (index === boardPage) {
+			return;
+		}
+		setCurrentPage(index);
+	};
 	return Array.from({ length: 7 }, (_, i) => boardPage - 3 + i).map(
 		(index) => {
-			if (index == boardPage) {
-				return (
-					<li key={index} className="page-item active">
-						<a className="page-link" href="#" tabIndex="-1">
-							{index}
-						</a>
-					</li>
-				);
-			} else if (index > 0 && index < totalPage) {
-				return (
-					<li key={index} className="page-item pointer">
-						<a
-							className="page-link"
-							onClick={() => setCurrentPage(index)}>
-							{index}
-						</a>
-					</li>
-				);
+			if (index < 1 || index >= totalPage) {
+				return null;
 			}
+			return (
+				<li
+					key={index}
+					className={`page-item ${index === boardPage && "active"}`}>
+					<button
+						className="page-link"
+						onClick={() => numberButtonOnClick(index)}>
+						{index}
+					</button>
+				</li>
+			);
 		}
 	);
 }
 
 export default function Pagination({ boardPage, totalCount, setCurrentPage }) {
 	const totalPage = totalCount / 20 + 1;
+	const prevButtonOnClick = () => {
+		if (boardPage > 1) {
+			setCurrentPage(boardPage - 1);
+		}
+	};
+	const nextButtonOnClick = () => {
+		if (boardPage < totalPage - 1) {
+			setCurrentPage(boardPage + 1);
+		}
+	};
 	return (
 		<nav className="p-4" aria-label="Page navigation">
 			<ul className="pagination justify-content-center">
 				<PreviousButtonLI
-					className={`page-item${boardPage > 1 ? "" : " disabled"}`}>
-					{boardPage > 1 ? (
-						<a
-							className="page-link pointer"
-							onClick={() => setCurrentPage(boardPage - 1)}>
-							이전
-						</a>
-					) : (
-						<a className="page-link" tabIndex="-1">
-							이전
-						</a>
-					)}
+					className={`page-item ${boardPage > 1 || "disabled"}`}>
+					<button className="page-link" onClick={prevButtonOnClick}>
+						이전
+					</button>
 				</PreviousButtonLI>
 				<Numbers
 					boardPage={boardPage}
@@ -52,19 +55,11 @@ export default function Pagination({ boardPage, totalCount, setCurrentPage }) {
 				/>
 				<NextButtonLI
 					className={`page-item ${
-						boardPage < totalPage - 1 ? "" : "disabled"
+						boardPage < totalPage - 1 || "disabled"
 					}`}>
-					{boardPage < totalPage - 1 ? (
-						<a
-							className="page-link pointer"
-							onClick={() => setCurrentPage(boardPage + 1)}>
-							다음
-						</a>
-					) : (
-						<a className="page-link" href="#" tabIndex="-1">
-							다음
-						</a>
-					)}
+					<button className="page-link" onClick={nextButtonOnClick}>
+						다음
+					</button>
 				</NextButtonLI>
 			</ul>
 		</nav>
